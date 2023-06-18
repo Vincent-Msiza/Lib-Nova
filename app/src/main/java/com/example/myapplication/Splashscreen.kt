10f8e63dde3e3c0789a4f6879f4b56cd614f6387
Vincent Mszia <jeanswagvince@gmail.com>
2023-06-19 00:31:36 +0200

    Final version for the splashscreen

---
+++ app/src/main/java/com/example/myapplication/Splashscreen.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+@SuppressLint("CustomSplashScreen")
+class Splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        // get reference to button
+
+        // Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+
+
+
+    }
+}
