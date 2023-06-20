9675cf217beecbea8b9f960010c297cf0de6f7bb
Vincent Mszia <jeanswagvince@gmail.com>
2023-06-21 01:34:55 +0200

    security features updated and use name saved through firestore

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var firestore: FirebaseFirestore
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        textView = findViewById(R.id.etSFullName)
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)
+
+        firestore = FirebaseFirestore.getInstance()
+        val db = Firebase.firestore
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+
+                    val name = document.getString("name")
+
+                    textView.text = "Hello, $name"
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting name", exception)
+            }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/Registration.kt
+++ app/src/main/java/com/example/myapplication/Registration.kt
@@ -1,14 +1,21 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Registration : AppCompatActivity() {
 
@@ -17,8 +24,10 @@ class Registration : AppCompatActivity() {
     private lateinit var etPass: EditText
     private lateinit var btnSignUp: Button
     lateinit var tvRedirectLogin: TextView
+    private lateinit var nameEditText: EditText
     // create Firebase authentication object
     private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +40,15 @@ class Registration : AppCompatActivity() {
         etPass = findViewById(R.id.etSPassword)
         btnSignUp = findViewById(R.id.btnSSigned)
         tvRedirectLogin = findViewById(R.id.tvRedirectLogin)
+        nameEditText = findViewById(R.id.etSFullName)
+
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)
 
         // Initialising auth object
         auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+        val db = Firebase.firestore
 
         btnSignUp.setOnClickListener {
             signUpUser()
@@ -53,6 +68,9 @@ class Registration : AppCompatActivity() {
         val email = etEmail.text.toString()
         val pass = etPass.text.toString()
         val confirmPassword = etConfPass.text.toString()
+        val name = nameEditText.text.toString()
+
+        val db = Firebase.firestore
 
         // check pass
         if (email.isBlank() || pass.isBlank() || confirmPassword.isBlank()) {
@@ -60,6 +78,13 @@ class Registration : AppCompatActivity() {
             return
         }
 
+         if (pass.length < 8) {
+            Toast.makeText(this, "Password must be at least 8 characters long", Toast.LENGTH_SHORT).show()
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Please enter a valid email address", Toast.LENGTH_SHORT).show()
+        }
         if (pass != confirmPassword) {
             Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT)
                 .show()
@@ -69,16 +94,36 @@ class Registration : AppCompatActivity() {
         // We call createUserWithEmailAndPassword
         // using auth object and pass the
         // email and pass in it.
-        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
-            if (it.isSuccessful) {
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) { task ->
 
-                val intent = Intent(this, RSuccess::class.java)
+            if (task.isSuccessful) {
+                // Registration successful
+                Toast.makeText(this, "Signed up success", Toast.LENGTH_SHORT).show()
+
+                val user = hashMapOf(
+                    "name" to name
+                )
+                // Add a new document with a generated ID
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(TAG, "Error adding document", e)
+                    }
+
+
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("name", name)
                 startActivity(intent)
+                finish()
+
 
             } else {
                 Toast.makeText(this, "Singed Up Failed!", Toast.LENGTH_SHORT).show()
-            }
-        }
+            }//closing else for is
+        }//closing the auth
 
 
 
